5c2e48d08ac20f2b8ffb541bc04d4590d296df66
000Zer000 <000Zer000@protonmail.com>
2021-02-01 18:38:48 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# QR-creator
+A python script which allows you to generate QR-codes, Any kind at ANY SIZE
+
+## Features
+
+1. More than 10 output formats:
+   
+    1. bmp
+       
+    2. ico
+       
+    3. jpeg
+       
+    4. png
+       
+    5. gif
+       
+    6. webp
+       
+    7. png
+    
+    Plus many other which are supported but not listed here
+       
+2. Easy to use
+   
+3. Also powerful
+   
+4. Hate the black color ? You pick the color!
+
+Want to have it ? Let's go for installation, don't worry it will be pretty easy
+
+## Installing
+
+Simply clone the repo:
+
+```shell
+git clone https://github.com/000Zer000/Password-Generator
+```
+Then install the dependencies:
+```shell
+pip install -r requirements.txt
+```
+
+## Usage
+
+simply run `python creator.py`, This script also allows for customization, Let's take a look at help banner:
+
+```text 
+  -h, --help            show this help message and exit
+  --fore-color FORE_COLOR, -fc FORE_COLOR Which color to use as the foreground color, defaults to 'black'
+  
+  --back-color BACK_COLOR, -bc BACK_COLOR Which color to use as the background color, defaults to 'white'
+  
+  --border BORDER, -b BORDER Increase or decrease the thickness of border, defaults to '4' and cannot be lower then
+  
+  --input INPUT, -in INPUT Instead of reading from STDIN, Read the content of the file
+  
+  --size SIZE, -s SIZE  Increase the size, defaults to whatever suits your data, the biggest is 40
+  
+  --output OUTPUT, -o OUTPUT Which file to store output as, defaults to 'Output'
+  
+  --kind KIND, -k KIND  What kind of picture do you want, defaults to 'png'
+```
+You can simply make a QR code with red foreground by this:
+```shell
+python creator.py -fc red
+```
+
+So You can easily play with the flags to generate your own QR code
+
+## Contacting me
+
+You can reach me by emailing me (`000Zer000@protonmail.com`)
+
+## License
+
+This repo is licensed under Apache 2.0, Read [COPYING](https://github.com/000Zer000/QR-creator/blob/main/COPYING) file for more info
---
+++ creator.py
@@ -0,0 +1,93 @@
+import sys
+import argparse
+import qrcode
+
+
+def get_input():
+    data = ""
+    z = ""
+    while True:
+        try:
+            z = input("Enter content, To end it, press Ctrl-Z on windows, or Ctrl-D on linux")
+            data += z
+        except EOFError:
+            data += z
+            break
+    return data
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fore-color", "-fc",
+                        help="Which color to use as the foreground color, defaults to 'black'",
+                        default="black", action="store",
+                        )
+    parser.add_argument("--back-color", "-bc",
+                        help="Which color to use as the background color, defaults to 'white'",
+                        default="white", action="store",
+                        )
+
+    parser.add_argument("--border", "-b",
+                        help="Increase or decrease the thickness of border, defaults to '4' and cannot be lower then",
+                        default=4, type=int, action="store",
+                        )
+
+    parser.add_argument("--input", "-in",
+                        help="Instead of reading from STDIN, Read the content of the file",
+                        default=None, action="store",
+                        )
+    parser.add_argument("--size", "-s",
+                        help="Increase the size, defaults to whatever suits your data, the biggest is 40",
+                        default=None, action="store", type=int,
+                        )
+    parser.add_argument("--output", "-o",
+                        help="Which file to store output as, defaults to 'Output'",
+                        default="Output", action="store",
+                        )
+    parser.add_argument("--kind", "-k",
+                        help="What kind of picture do you want, defaults to 'png'",
+                        default="png", action="store",
+                        )
+
+    ns = parser.parse_args(args)
+    if ns.border < 4:
+        print("Boarder cannot be lower then 4, going with 4")
+        ns.border = 4
+    if ns.size is None:
+        print("True")
+    elif 0 > ns.size > 40:
+        print("'Size' cannot be lower then 1 or bigger then 40, going with the most suitable for your data")
+        ns.size = None
+
+    qr = qrcode.QRCode(version=ns.size, border=ns.border)
+    if ns.input:
+        try:
+            with open(ns.input, "r") as f:
+                data = f.read()
+        except FileNotFoundError:
+            print("Cannot open '{}', File doesn't exist".format(ns.input))
+            return
+        except PermissionError:
+            print("Cannot open '{}', Insufficient permissions".format(ns.input))
+    else:
+        data = get_input()
+    try:
+        qr.add_data(data)
+        qr.make()
+        factory = qr.make_image(fill_color=ns.fore_color, back_color=ns.back_color)
+        factory.kind = ns.kind
+    except:
+        print("Unhandled exception happened, Please report this on github")
+        raise
+    try:
+        factory.save(ns.output + "." + ns.kind)
+    except KeyError:
+        print("Unknown format '{}', falling back on 'png'".format(ns.kind))
+        factory.save(ns.output + ".png", format="png")
+
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print("User requested exit, exiting...")
